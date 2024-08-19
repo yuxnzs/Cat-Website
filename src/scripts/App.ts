279e733648a585ab85c724cc5979cac9f0ca9cd7
yuxnzs <yuchengliu0429@gmail.com>
2024-08-19 17:38:12 +0800

    Init project

---
+++ src/scripts/App.ts
@@ -0,0 +1,15 @@
+import Cat from "./Cat.js";
+import API_KEY from "./Config.js";
+
+declare var $: any;
+
+// $.ajax({
+//   url: "https://api.thecatapi.com/v1/images/search?limit=30&has_breeds=1&size=small",
+//   method: "GET",
+//   headers: {
+//     "x-api-key": API_KEY,
+//   },
+//   success: function (data: Cat[]) {
+//     console.log(data);
+//   },
+// });
